72b7da4febf4d2c095e4fdfcf38f0bdc46321999
Hei Wiper <heiwiper@gmail.com>
2020-08-31 13:38:38 +0100

    Merged the two maps in one

--- game.py
+++ game.py
@@ -1,76 +1,85 @@
 import pygame
 from PIL import Image, ImageOps
+
 from first_window import First_window
 
-def draw_map(map1_color, map1, map2):
-    im = [
-    [[],[],[]],
-    [[],[],[]],
-    [[],[],[]]
-    ]
-    for i in range(3):
-        for j in range(3):
-                if map1_color[i][j] == -1:
-                    im[i][j] = Image.open('assets/map/red_'+str(i+1)+str(j+1)+'.png')
-                elif map1_color[i][j] == 0:
-                    im[i][j] = Image.open('assets/map/original_'+str(i+1)+str(j+1)+'.png')
-                else :
-                    im[i][j] = Image.open('assets/map/blue_'+str(i+1)+str(j+1)+'.png')
+width = 1280
+height =960
+mapColumns = 7
+mapRows = 3
 
-    for i in range(3):
-        for j in range(3):
-            map1.paste(im[i][j], (187*j, 149*i))
 
-    map2_color = map1_color.copy()
-    map2_color.reverse()
-    for i in range(3):
-        for j in range(3):
-                if map2_color[i][j] == -1:
-                    im[i][j] = Image.open('assets/map/red_'+str(i+1)+str(j+1)+'.png')
-                elif map2_color[i][j] == 0:
-                    im[i][j] = Image.open('assets/map/original_'+str(i+1)+str(j+1)+'.png')
-                else :
-                    im[i][j] = Image.open('assets/map/blue_'+str(i+1)+str(j+1)+'.png')
+def draw_map(mapColors, map):
+    imageMap = []
+    for r in range(mapRows):
+        tempList = []
+        for c in range(mapColumns):
+            tempList.append(None)
+        imageMap.append(tempList)
 
-    for i in range(3):
-        for j in range(3):
-            map2.paste(im[i][j], (187*j, 149*i))
+    for r in range(mapRows):
+        for c in range(mapColumns):
+            if mapColors[r][c] == 1:
+                imageMap[r][c] = Image.open('assets/map/red_rect.png')
+            elif mapColors[r][c] == -1:
+                imageMap[r][c] = Image.open('assets/map/blue_rect.png')
+            else:
+                imageMap[r][c] = Image.open('assets/map/frontier.png')
 
-def display_numbers(number_troops, firstTroopsP1, secondTroopsP1, thirdTroopsP1, firstTroopsP2, secondTroopsP2, thirdTroopsP2):
+    for r in range(mapRows):
+        for c in range(mapColumns):
+            map.paste(imageMap[r][c], (145*c, 230*r))
+
+
+def display_numbers(number_troops, player1Troops, player2Troops):
     font = pygame.font.SysFont('Arial', 40)
     font.set_bold(True)
 
-    remaining_troopsP1 = font.render(str(number_troops-(firstTroopsP1+secondTroopsP1+thirdTroopsP1)), False, (47,60,126))
-    remaining_troopsP2 = font.render(str(number_troops-(firstTroopsP2+secondTroopsP2+thirdTroopsP2)), False, (158,15,15))
-    firstTroopsP1_text = font.render(str(firstTroopsP1), False, (47,60,126))
-    secondTroopsP1_text = font.render(str(secondTroopsP1), False, (47,60,126))
-    thirdTroopsP1_text = font.render(str(thirdTroopsP1), False, (47,60,126))
-    firstTroopsP2_text = font.render(str(firstTroopsP2), False, (158,15,15))
-    secondTroopsP2_text = font.render(str(secondTroopsP2), False, (158,15,15))
-    thirdTroopsP2_text = font.render(str(thirdTroopsP2), False, (158,15,15))
+    remaining_troopsP1 = font.render(str(number_troops-(player1Troops[0]+player1Troops[1]+player1Troops[2])), False, (47,60,126))
+    remaining_troopsP2 = font.render(str(number_troops-(player2Troops[0]+player2Troops[1]+player2Troops[2])), False, (158,15,15))
+    player1TroopsText = []
+    player2TroopsText = []
+    for n in range(number_troops):
+        player1TroopsText.append(font.render(str(player1Troops[n]), False, (158,15,15)))
+        player2TroopsText.append(font.render(str(player2Troops[n]), False, (47,60,126)))
+
     screen.blit(remaining_troopsP1, (35, 780))
     screen.blit(remaining_troopsP2, (685, 780))
-    screen.blit(firstTroopsP1_text, (90, 280))
-    screen.blit(secondTroopsP1_text, (90+187, 280))
-    screen.blit(thirdTroopsP1_text, (90+187*2, 280))
-    screen.blit(firstTroopsP2_text, (740, 280))
-    screen.blit(secondTroopsP2_text, (740+187, 280))
-    screen.blit(thirdTroopsP2_text, (740+187*2, 280))
+    for i in range(number_troops):
+        screen.blit(player1TroopsText[i], (60, 140+230*i))
+        screen.blit(player2TroopsText[i], (width-80, 140+230*i))
 
 # display the first window
 first_window = First_window()
 number_troops = first_window.begin()
+
+
+def claim_territory(player, row):
+    for c in range(mapColumns):
+        if mapColors[row][c] == 0:
+            if player == 1:
+                mapColors[row][c] = 1
+                if c != mapColumns-1:
+                    mapColors[row][c+1] = 0
+                return
+            elif player == 2:
+                mapColors[row][c] = -1
+                if c != 0:
+                    mapColors[row][c-1] = 0
+                return
+
 
 # init
 pygame.init()
 pygame.font.init()
 
 # set the size of the window
-size = screenWidth, screenHeight = 1276, 849
+size = screenWidth, screenHeight = width, height
 screen = pygame.display.set_mode(size)
 
 # set the background of the first window
 background = pygame.image.load('assets/background.png')
+mapImage = pygame.image.load('assets/map/full_map.png')
 
 # set title of the window
 pygame.display.set_caption("THJ Game")
@@ -79,34 +88,39 @@ pygame.display.set_caption("THJ Game")
 # music = pygame.mixer.music.load("./assets/sounds/bgm.wav")
 # pygame.mixer.music.play(-1)
 
-map1_color = [
-[-1, -1, -1], # red
-[0, 0, 0], # original
-[1, 1, 1] # blue
-]
-map1 = Image.new('RGB', (561, 447))
-map2 = Image.new('RGB', (561, 447))
+mapColors = []
+for r in range(mapRows):
+    tempList = []
+    for c in range(mapColumns):
+        if c < mapColumns//2:
+            tempList.append(1)
+        elif c == mapColumns//2:
+            tempList.append(0)
+        else:
+            tempList.append(-1)
+    mapColors.append(tempList)
 
-firstTroopsP1 = 0
-secondTroopsP1 = 0
-thirdTroopsP1 = 0
-firstTroopsP2 = 0
-secondTroopsP2 = 0
-thirdTroopsP2 = 0
-number_troops_P1 = number_troops_P2 = number_troops
+map = Image.new('RGBA', (1015, 690))
+
+player1Troops = []
+player2Troops = []
+for i in range(mapRows):
+    player1Troops.append(0)
+    player2Troops.append(0)
+numberTroopsP1 = numberTroopsP2 = number_troops
 gain1 = 0
 gain2 = 0
 winner = None
+
 # main loop
 run = True
 while run:
     screen.blit(background, (0, 0))
-    draw_map(map1_color, map1, map2)
-    py_map1 =  pygame.image.fromstring(map1.tobytes(), (561, 447), 'RGB')
-    py_map2 =  pygame.image.fromstring(map2.tobytes(), (561, 447), 'RGB')
-    screen.blit(py_map1, (30, 70))
-    screen.blit(py_map2, (680, 70))
-    display_numbers(number_troops, firstTroopsP1, secondTroopsP1, thirdTroopsP1, firstTroopsP2, secondTroopsP2, thirdTroopsP2)
+    draw_map(mapColors, map)
+    py_map =  pygame.image.fromstring(map.tobytes(), (1015, 690), 'RGBA')
+    screen.blit(mapImage, (132, 30))
+    screen.blit(py_map, (132, 30))
+    display_numbers(number_troops, player1Troops, player2Troops)
     if winner!=None :
         if gain1 == gain2:
             screen.blit(winner, (420, 250))
@@ -119,90 +133,90 @@ while run:
             run = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            if 33<pos[0]<220 and 220<pos[1]<309:
+            if 0<pos[0]<132 and 30<pos[1]<260:
                 if event.button == 1 : # LEFT CLICK
-                    if(number_troops_P1>0):
-                        number_troops_P1-=1
-                        firstTroopsP1+=1
+                    if(numberTroopsP1>0):
+                        numberTroopsP1-=1
+                        player1Troops[0]+=1
                 else :
-                    if(firstTroopsP1>0):
-                        firstTroopsP1-=1
-                        number_troops_P1+=1
-            elif 220<pos[0]<407 and 220<pos[1]<309:
+                    if(player1Troops[0]>0):
+                        player1Troops[0]-=1
+                        numberTroopsP1+=1
+            elif 0<pos[0]<132 and 260<pos[1]<490:
                 if event.button == 1 :
-                    if(number_troops_P1>0):
-                        number_troops_P1-=1
-                        secondTroopsP1+=1
+                    if(numberTroopsP1>0):
+                        numberTroopsP1-=1
+                        player1Troops[1]+=1
                 else :
-                    if(secondTroopsP1>0):
-                        secondTroopsP1-=1
-                        number_troops_P1+=1
-            elif 407<pos[0]<594 and 220<pos[1]<309:
+                    if(player1Troops[1]>0):
+                        player1Troops[1]-=1
+                        numberTroopsP1+=1
+            elif 0<pos[0]<132 and 490<pos[1]<720:
                 if event.button == 1 :
-                    if(number_troops_P1>0):
-                        number_troops_P1-=1
-                        thirdTroopsP1+=1
+                    if(numberTroopsP1>0):
+                        numberTroopsP1-=1
+                        player1Troops[2]+=1
                 else :
-                    if(thirdTroopsP1>0):
-                        thirdTroopsP1-=1
-                        number_troops_P1+=1
-            elif 683<pos[0]<870 and 220<pos[1]<309:
+                    if(player1Troops[2]>0):
+                        player1Troops[2]-=1
+                        numberTroopsP1+=1
+            elif 1148<pos[0]<width and 30<pos[1]<260:
                 if event.button == 1 :
-                    if(number_troops_P2>0):
-                        number_troops_P2-=1
-                        firstTroopsP2+=1
+                    if(numberTroopsP2>0):
+                        numberTroopsP2-=1
+                        player2Troops[0]+=1
                 else :
-                    if(firstTroopsP2>0):
-                        firstTroopsP2-=1
-                        number_troops_P2+=1
-            elif 870<pos[0]<1057 and 220<pos[1]<309:
+                    if(player2Troops[0]>0):
+                        player2Troops[0]-=1
+                        numberTroopsP2+=1
+            elif 1148<pos[0]<width and 260<pos[1]<490:
                 if event.button == 1 :
-                    if(number_troops_P2>0):
-                        number_troops_P2-=1
-                        secondTroopsP2+=1
+                    if(numberTroopsP2>0):
+                        numberTroopsP2-=1
+                        player2Troops[1]+=1
                 else :
-                    if(secondTroopsP2>0):
-                        secondTroopsP2-=1
-                        number_troops_P2+=1
-            elif 1057<pos[0]<1244 and 220<pos[1]<309:
+                    if(player2Troops[1]>0):
+                        player2Troops[1]-=1
+                        numberTroopsP2+=1
+            elif 1148<pos[0]<width and 490<pos[1]<720:
                 if event.button == 1 :
-                    if(number_troops_P2>0):
-                        number_troops_P2-=1
-                        thirdTroopsP2+=1
+                    if(numberTroopsP2>0):
+                        numberTroopsP2-=1
+                        player2Troops[2]+=1
                 else :
-                    if(thirdTroopsP2>0):
-                        thirdTroopsP2-=1
-                        number_troops_P2+=1
+                    if(player2Troops[2]>0):
+                        player2Troops[2]-=1
+                        numberTroopsP2+=1
             elif 540<pos[0]<737 and 593<pos[1]<656: # ISSUE DU JEU
-                if firstTroopsP1 > firstTroopsP2:
+                if player1Troops[0] > player2Troops[0]:
                     gain1+=1
                     gain2-=1
-                    map1_color[1][0] = 1
-                elif firstTroopsP1 < firstTroopsP2:
+                    claim_territory(1, 0)
+                elif player1Troops[0] < player2Troops[0]:
                     gain1-=1
                     gain2+=1
-                    map1_color[1][0] = -1
-                if secondTroopsP1 > secondTroopsP2:
+                    claim_territory(2, 0)
+                if player1Troops[1] > player2Troops[1]:
                     gain1+=1
                     gain2-=1
-                    map1_color[1][1] = 1
-                elif secondTroopsP1 < secondTroopsP2:
+                    claim_territory(1, 1)
+                elif player1Troops[1] < player2Troops[1]:
                     gain1-=1
                     gain2+=1
-                    map1_color[1][1] = -1
-                if thirdTroopsP1 > thirdTroopsP2:
+                    claim_territory(2, 1)
+                if player1Troops[2] > player2Troops[2]:
                     gain1+=1
                     gain2-=1
-                    map1_color[1][2] = 1
-                elif thirdTroopsP1 < thirdTroopsP2:
+                    claim_territory(1, 2)
+                elif player1Troops[2] < player2Troops[2]:
                     gain1-=1
                     gain2+=1
-                    map1_color[1][2] = -1
+                    claim_territory(2, 2)
                 if gain1 > gain2:
                     winner = pygame.image.load("assets/blueWin.png")
                 elif gain1 < gain2:
                     winner =  pygame.image.load("assets/redWin.png")
-                else :
+                else:
                     font = pygame.font.SysFont('Arial', 100)
                     font.set_bold(True)
                     winner = font.render('Match nul !', False, (204,255,153))
